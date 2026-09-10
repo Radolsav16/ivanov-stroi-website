2ae4c9aef0e685dc31e93c049f4413eaa5d07f6a
Radoslav Todorov <todorovradoslav18@gmail.com>
2026-09-10 21:30:37 +0300

    fix: correct ASP.NET Core header configuration

--- ProjectsApi/Program.cs
+++ ProjectsApi/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    options.KnownNetworks.Clear();
+    options.KnownIPNetworks.Clear();
     options.KnownProxies.Clear();
 });
 builder.Services.AddCors(options =>
